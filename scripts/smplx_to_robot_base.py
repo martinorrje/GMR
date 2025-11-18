23caaf5666738daf69f3fd501e5f491930ac89e4
martinorrje <ma.orrje@gmail.com>
2025-11-18 16:28:30 +0100

    Added base script

---
+++ scripts/smplx_to_robot_base.py
@@ -0,0 +1,109 @@
+import argparse
+import pathlib
+import os
+import time
+
+import numpy as np
+
+from general_motion_retargeting import GeneralMotionRetargeting as GMR
+from general_motion_retargeting import RobotMotionViewer
+from general_motion_retargeting.utils.smpl import load_smplx_file, get_smplx_data_offline_fast
+
+from rich import print
+
+if __name__ == "__main__":
+    
+    HERE = pathlib.Path(__file__).parent
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--smplx_file",
+        help="SMPLX motion file to load.",
+        required=True,
+        type=str,
+    )
+    
+    parser.add_argument(
+        "--robot",
+        choices=["pebble"],
+        default="pebble",
+    )
+    
+    parser.add_argument(
+        "--save_path",
+        default=None,
+        help="Path to save the robot motion.",
+    )
+    
+    parser.add_argument(
+        "--loop",
+        default=False,
+        action="store_true",
+        help="Loop the motion.",
+    )
+
+    parser.add_argument(
+        "--record_video",
+        default=False,
+        action="store_true",
+        help="Record the video.",
+    )
+
+    parser.add_argument(
+        "--rate_limit",
+        default=False,
+        action="store_true",
+        help="Limit the rate of the retargeted robot motion to keep the same as the human motion.",
+    )
+
+    args = parser.parse_args()
+
+
+    SMPLX_FOLDER = HERE / ".." / "assets" / "body_models"
+    
+    
+    # Load SMPLX trajectory
+    smplx_data, body_model, smplx_output, actual_human_height = load_smplx_file(
+        args.smplx_file, SMPLX_FOLDER
+    )
+    
+    # align fps
+    tgt_fps = 30
+    smplx_data_frames, aligned_fps = get_smplx_data_offline_fast(smplx_data, body_model, smplx_output, tgt_fps=tgt_fps)
+    
+   
+    # Initialize the retargeting system
+    retarget = GMR(
+        actual_human_height=actual_human_height,
+        src_human="smplx",
+        tgt_robot=args.robot,
+    )
+    
+    curr_frame = 0
+    # FPS measurement variables
+    fps_counter = 0
+    fps_start_time = time.time()
+    fps_display_interval = 2.0  # Display FPS every 2 seconds
+    
+    # Start the viewer
+    i = 0
+
+    while True:
+        i += 1
+        if i >= len(smplx_data_frames):
+            break
+    
+        # FPS measurement
+        fps_counter += 1
+        current_time = time.time()
+        if current_time - fps_start_time >= fps_display_interval:
+            actual_fps = fps_counter / (current_time - fps_start_time)
+            print(f"Actual rendering FPS: {actual_fps:.2f}")
+            fps_counter = 0
+            fps_start_time = current_time
+        
+        # Update task targets.
+        smplx_data = smplx_data_frames[i]
+
+        # retarget
+        qpos = retarget.retarget(smplx_data)
